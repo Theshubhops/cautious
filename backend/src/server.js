f0045a4612a89fb45082d9a45fff9f902ceb3d89
shubham.techspace <shubham.techspace@gmail.com>
2025-09-17 12:47:51 +0000

    Update

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,35 +1,35 @@
 import express from "express";
+import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
-import path from "path";
 import cors from "cors";
+import { createServer } from "http";
 
 import authRoutes from "./routes/auth.route.js";
 import messageRoutes from "./routes/message.route.js";
+
 import { connectDB } from "./lib/db.js";
-import { ENV } from "./lib/env.js";
-import { app, server } from "./lib/socket.js";
+import { app, server, io } from "./lib/socket.js";
 
-const __dirname = path.resolve();
+dotenv.config();
+const PORT = process.env.PORT || 3000;
 
-const PORT = ENV.PORT || 3000;
-
-app.use(express.json({ limit: "5mb" })); // req.body
-app.use(cors({ origin: ENV.CLIENT_URL, credentials: true }));
+// MIDDLEWARES
+app.use(express.json({ limit: "5mb" }));
+app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
+const corsOptions = {
+	origin: ["http://localhost:5173", "http://127.0.0.1:5173"],
+	credentials: true,
+};
+app.use(cors(corsOptions));
+
+// ROUTES
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 
-// make ready for deployment
-if (ENV.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "../frontend/dist")));
-
-  app.get("*", (_, res) => {
-    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
-  });
-}
-
 server.listen(PORT, () => {
-  console.log("Server running on port: " + PORT);
-  connectDB();
+	process.env.MONGO_URI = process.env.VITE_MONGO_URI;
+	connectDB();
+	console.log(`Server running on port: ${PORT}`);
 });
